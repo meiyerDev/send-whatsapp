6d96638a1a3c439a3ca16bb7dacfb0e4f8d804db
Meiyer jaimes <meiyerjaimes@outlook.com>
2022-12-22 14:32:05 -0400

    feat: add PWA and Seo settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,46 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/send-whatsapp/',
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'vite.svg'],
+      manifest: {
+        name: 'Send a WhatsApp',
+        short_name: 'SendWhatsApp',
+        description: 'Send a WhatsApp without saving the contact.',
+        theme_color: '#f3f4f6',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
