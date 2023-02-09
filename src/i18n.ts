672c90d3caba1aa85edee81402c1f36feda3e1d1
Meiyer jaimes <meiyerjaimes@outlook.com>
2023-02-09 00:02:10 -0500

    refactor: locales file

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,22 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+import translationEN from './locales/en/translation.json';
+import translationES from './locales/es/translation.json';
+
+// the translations
+const resources = {
+  en: { translation: translationEN },
+  es: { translation: translationES },
+};
+
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
+    load: 'languageOnly',
     fallbackLng: 'en',
     debug: true,
 
